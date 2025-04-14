7f562176da5ce1f2b2877b65c519e547887b3e8a
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-14 16:38:32 -0400

    arrays

--- python basico/arrays.py
+++ python basico/arrays.py
@@ -1,6 +1,35 @@
 #arrays (listas)#
 array = [1,2,3,4,5];
+string = ["um","dois","três","quatro","cinco"];
+decimais = [1.0,2.0,3.0,4.0,5.0];
+
 array[0] = 10;
 
 print(array[0]);
 print(array[1]);
+print(decimais);
+
+arrayVazio = [];
+arrayVazio.append("Hello")
+arrayVazio.append("World")
+print(arrayVazio);
+
+'''
+append(item) - Adiciona um item ao final da lista.
+insert(posição, item) - Adiciona um item na posição especificada.
+pop() - Remove o último item da lista e retorna o valor.
+pop(posição) - Remove o item na posição especificada e retorna o valor.
+sort() - Ordena a lista em ordem crescente.
+reverse() - Inverte a ordem dos elementos na lista.
+index(item) - Retorna a posição do item na lista.
+count(item) - Retorna a quantidade de vezes que o item aparece na lista.
+remove(item) - Remove o item da lista.
+extend(array) - Adiciona os elementos de outra lista ao final da lista.
+copy() - Cria uma cópia da lista.
+clear() - Remove todos os elementos da lista.
+len(array) - Retorna o tamanho da lista.
+min(array) - Retorna o menor valor da lista.
+max(array) - Retorna o maior valor da lista.
+sum(array) - Retorna a soma dos elementos da lista.
+
+'''
