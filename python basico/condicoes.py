0a9e72eb1dbe3b1a34761f8fc8c4432700f67ceb
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-14 16:28:44 -0400

    primeiro commit

---
+++ python basico/condicoes.py
@@ -0,0 +1,5 @@
+#condiçoes#
+ingressos = 50;
+comprador = 250;
+tem_ingresso = (ingressos >= comprador);
+print(tem_ingresso);
---
+++ python basico/main.py
@@ -0,0 +1,36 @@
+print("Hello World");
+#print(1+1);
+
+
+
+
+
+#operadores#
+soma = 1 + 1;
+subtracao = 1 - 1;
+multiplicacao = 1 * 1;
+divisao = 1 / 1;
+potencia = 2 ** 3;
+
+'''
+print("Soma:",soma);
+print("Subtração:",subtracao);
+print("Multiplicação:",multiplicacao);
+print("Divisão:",divisao);
+print("Potência:",potencia);
+'''
+
+#if else#
+if idade >= 18:
+    print("Você é maior de idade.");
+elif idade <=0:
+    print("Idade inválida.");
+else:
+    print("Você é menor de idade.");
+
+#arrays (listas)#
+array = [1,2,3,4,5];
+array[0] = 10;
+
+print(array[0]);
+print(array[1]);
