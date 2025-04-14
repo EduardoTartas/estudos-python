7c9222720f37c039ea6f80fa73e5056917ad7e5e
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-14 16:30:29 -0400

    if-else

---
+++ python basico/if-else.py
@@ -0,0 +1,7 @@
+#if else#
+if idade >= 18:
+    print("Você é maior de idade.");
+elif idade <=0:
+    print("Idade inválida.");
+else:
+    print("Você é menor de idade.");
--- python basico/main.py
+++ python basico/main.py
@@ -7,13 +7,7 @@ print("Hello World");
 
 
 
-#if else#
-if idade >= 18:
-    print("Você é maior de idade.");
-elif idade <=0:
-    print("Idade inválida.");
-else:
-    print("Você é menor de idade.");
+
 
 #arrays (listas)#
 array = [1,2,3,4,5];
