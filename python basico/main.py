661feb61253f5ec846007f767c0455932d096c7f
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-14 16:30:00 -0400

    operadores

--- python basico/main.py
+++ python basico/main.py
@@ -5,20 +5,7 @@ print("Hello World");
 
 
 
-#operadores#
-soma = 1 + 1;
-subtracao = 1 - 1;
-multiplicacao = 1 * 1;
-divisao = 1 / 1;
-potencia = 2 ** 3;
 
-'''
-print("Soma:",soma);
-print("Subtração:",subtracao);
-print("Multiplicação:",multiplicacao);
-print("Divisão:",divisao);
-print("Potência:",potencia);
-'''
 
 #if else#
 if idade >= 18:
