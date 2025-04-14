df3c03735777f53dc4f1701d2f72cd9a11d43208
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-14 16:29:12 -0400

    inputs

---
+++ python basico/inputs.py
@@ -0,0 +1,7 @@
+#input (só recebe string)#
+nome = input("Qual o seu nome? ");
+idade = int(input("Qual a sua idade? "));
+
+print("Seu nome é",nome,"e sua idade é",idade,"anos.");
+
+#exit(0); (encerra o código)
