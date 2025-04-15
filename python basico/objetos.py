109c53928c5cca92ad888250960bbb109a9ac15f
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-15 12:10:26 -0400

    objetos python

---
+++ python basico/objetos.py
@@ -0,0 +1,12 @@
+pessoa = {"nome": "Eduardo", "idade": 19, "altura": 1.75};
+
+print("Nome:", pessoa["nome"], "\nIdade:", pessoa["idade"], "\nAltura:", pessoa["altura"]);
+
+npcs = [
+{"nome": "Gustavo", "idade": 20, "altura": 1.80},
+{"nome": "Carlos", "idade": 22, "altura": 1.85},
+{"nome": "Ana", "idade": 19, "altura": 1.70},
+{"nome": "Maria", "idade": 21, "altura": 1.65}
+];
+
+print(npcs[0]["nome"]);
