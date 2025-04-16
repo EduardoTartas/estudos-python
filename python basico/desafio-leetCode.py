9a7bb552a2a4da1955b1b6d0b1cb251b8f8c5446
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-16 16:35:15 -0400

    ronan to int

--- python basico/desafio-leetCode.py
+++ python basico/desafio-leetCode.py
@@ -9,8 +9,9 @@ class Solution(object):
                 
 
 
-    print(twoSum(0,[3,2,3], 6))
+    #print(twoSum(0,[3,2,3], 6))
 
+#---------
 class Solution(object):
     def isPalindrome(self, x):
         x = str(x)
@@ -19,4 +20,32 @@ class Solution(object):
         else:
             return False
 
-    print(isPalindrome(0, 121))
+    #print(isPalindrome(0, 121))
+
+#---------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        for char in reversed(s):
+            current_value = roman_map[char]
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            prev_value = current_value
+        
+        return total
+
+  
+    print(romanToInt(0,"III"))
