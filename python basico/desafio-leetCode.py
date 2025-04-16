f5151104cff473ba4fd99d40116e7515169d0679
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-16 16:54:39 -0400

    longestCommonPrefix

--- python basico/desafio-leetCode.py
+++ python basico/desafio-leetCode.py
@@ -48,4 +48,25 @@ class Solution(object):
         return total
 
   
-    print(romanToInt(0,"III"))
+    #print(romanToInt(0,"III"))
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        for string in strs[1:]:
+            while not string.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
+
+# Example usage
+solution = Solution()
+print(solution.longestCommonPrefix(["flower", "flow", "flight"]))
